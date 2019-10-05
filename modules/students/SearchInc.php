0d2ae2657f015092773419072f973406e911045e
root <root@experimental>
2019-10-05 06:24:12 +0530

    Script to create class created
    It takes data from form and creates database

--- modules/students/SearchInc.php
+++ modules/students/SearchInc.php
@@ -195,7 +195,9 @@ if ($_REQUEST['search_modfunc'] == 'search_fnc' || !$_REQUEST['search_modfunc'])
 //
 //            PopTable('footer', $extra['footer'] . $extra_footer);
 //	    echo '<input type="button" value="Create Admission Form" onclick="location.href=\'https://google.com\'" />';
-    	    echo '<a href="http://localhost/sudo-su/formtools/index.php" target="_blank">Create Admission Form</a>';
+	    echo '<a href="http://localhost/sudo-su/formtools/index.php" target="_blank">Create Admission Form</a>';
+	    echo '<br><form action="create_classes.php">
+	        <input type="submit" value="Create Classes"></input></form>';
     	    echo '</FORM>';
             // set focus to last name text box
 	    echo '<script type="text/javascript">
